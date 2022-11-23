80348030e903c224f878a153cdf98f7097ac6e62
Andrey Glumov <gluman@yandex.ru>
2022-11-23 21:55:28 +0300

    v 0.23112022_1

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import datetime as d
 import requests
 from pprint import pprint as pp
 
+
 class VK:
 
     def __init__(self, access_token, user_id, version='5.131'):
@@ -21,7 +22,6 @@ class VK:
         return response.json()
 
     def fotos_get(self, f_count, user_id):
-
         url = vk_link + 'photos.get'
         params = {'owner_id': user_id,
                   'album_id': 'profile',
@@ -37,6 +37,8 @@ class Yandex:
     """
     class for yandex objects and methods
     """
+    base_host = ya_link
+
     def __init__(self, token):
         self.token = token
 
@@ -48,8 +50,9 @@ class Yandex:
 
     def upload_fotos(self):
         pass
+
     def _get_upload_link(self, path):
-        uri = '/v1/disk/resource/upload/'
+        uri = 'v1/disk/resources/upload/'
         request_url = self.base_host + uri
         params = {'path': path,
                   'overwrite': True}
@@ -58,12 +61,12 @@ class Yandex:
         return response.json()['href']
 
     def create_ya_folder(self):
-        uri = '/v1/disk/resource/'
+        uri = 'v1/disk/resources/'
         request_url = self.base_host + uri
-        path = str('/backup/' + vk_user_id +'_'+ d.utcnow)
+        path = str(vk_user_id) + '_' + str(d.date.today())
         params = {'path': path}
         response = requests.put(request_url, params=params, headers=self.get_headers())
-        if response.status_code == 201:
+        if response.status_code == 201 or response.status_code == 409:
             return path
         else:
             pp(response.json())
@@ -71,10 +74,10 @@ class Yandex:
     def check_exist_file(self, file_name):
         pass
 
-    def upload_to_ya(self,  url):
-        uri = '/v1/disk/resource/upload/'
+    def upload_to_ya(self, url, file_name):
+        uri = 'v1/disk/resources/upload/'
         request_url = self.base_host + uri
-        ya_path = self.create_ya_folder()
+        ya_path = self.create_ya_folder() + '/' + file_name
         if type(ya_path) is str:
             params = {'url': url, 'path': ya_path}
             response = requests.post(request_url, params=params, headers=self.get_headers())
@@ -82,25 +85,26 @@ class Yandex:
             return
 
 
-def upload_from_vk_to_ya(vk_json, ya_obj, fotos_count):
-    count = 0 # счетчие количества фото
+def upload_from_vk_to_ya(vk_json, fotos_count=5):
+    count = 1  # счетчик количества фото
     upload_fotos_list = []
     upload_fotos_dict = {}
     for item in vk_json['response']['items']:
-        if count < fotos_count:
+        if count <= fotos_count:
             upload_fotos_dict['file name'] = str(item['likes']['count']) + '.jpeg'
-            for foto in item['sizes']:
-                if foto['type'] == 'w':
-                    upload_fotos_dict['upload_link'] = foto['url']
-                    upload_fotos_dict['size'] = foto['type']
+            for size in item['sizes']:
+                if size['type'] == 'w':
+                    upload_fotos_dict['upload_link'] = size['url']
+                    upload_fotos_dict['size'] = size['type']
                     upload_fotos_list.append(upload_fotos_dict)
-                    count += 1
-                    ya.upload_to_ya(upload_fotos_dict['upload_link'])
-
-
-
-
-
+                elif size['type'] == 'z':
+                    upload_fotos_dict['upload_link'] = size['url']
+                    upload_fotos_dict['size'] = size['type']
+                    upload_fotos_list.append(upload_fotos_dict)
+                else
+                    size = dict(sorted(item['size']))
+            count += 1
+            ya.upload_to_ya(upload_fotos_dict['upload_link'], upload_fotos_dict['file name'])
 
 
 if __name__ == '__main__':
@@ -108,14 +112,12 @@ if __name__ == '__main__':
     # ya_token = input('Введите ключ, полученный с полигона Yandex:')
     # access_token = 'access_token'
     # user_id = 'id29252022'
-    nic_name = 'glumovav' # my account
-    foto_count = 5 #
+    nic_name = 'glumovav'  # my account
+    foto_count = 4  #
     vk = VK(access_token, nic_name)
     ya = Yandex(ya_token)
     pp(vk.users_info())
     vk_user = vk.users_info()
     vk_user_id = vk_user['response'][0]['id']
     res_fotos = vk.fotos_get(foto_count, vk_user_id)
-    upload_from_vk_to_ya(res_fotos, ya, foto_count)
-
-
+    upload_from_vk_to_ya(res_fotos, foto_count)
