6fa70e055477bd955b5a7c5fb1749758109fe1c6
Glumov Andrey <gluman@yandex.ru>
2023-03-30 18:27:44 +0300

    30.03.2023 main commit before add tests

---
+++ settings_main.py
@@ -0,0 +1,8 @@
+# yandex base link
+base_ya_link = 'https://cloud-api.yandex.net/'
+# yandex token
+Y_TOKEN = 'y0_AgAAAAAAXATVAADLWwAAAADTaJSZ6YPhHSFgQF2GMMzMJaka26tLGyI'
+# Service access VK key
+base_vk_link = 'https://api.vk.com/method/'
+# vk acces token
+VK_TOKEN = 'vk1.a.-qOwQI0VneX7tY3jZgNShxBqO75KpX9EgXXBuwlwQSrCcPVJy5x00BQ80_rH-EPR4Hnr7cl0mymlybo8P7_ZhoK7nP1kK_BgPHUxIHGWcZ2_RLEXWrS5vpbrdTzsb2ZtfJuG2_3XHfMafVD7scnzNfY8d008se3XnsU2IilGjTMXrajOaKxS3NS-vRGRsPOo6zW81YhQ-EQfv0bCY-V3Rg'
